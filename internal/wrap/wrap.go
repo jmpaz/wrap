ba16c52661797a94e71e04681ff74166e4b1a2ac
jmpaz <30947643+jmpaz@users.noreply.github.com>
2025-03-02 19:39:53 -0500

    Ensure ≤1 trailing newlines before closing fence/tag

--- internal/wrap/wrap.go
+++ internal/wrap/wrap.go
@@ -16,21 +16,26 @@ func WrapContent(content string, format string) string {
 	case "md":
 		return wrapMD(content)
 	default:
-		return content // or error, but let's just return content.
+		return content
 	}
 }
 
+func trimTrailingNewlines(s string) string {
+	return strings.TrimRight(s, "\n")
+}
+
 func wrapXML(content string) string {
+	content = trimTrailingNewlines(content)
 	return fmt.Sprintf("<paste>\n%s\n</paste>\n", content)
 }
 
 func wrapMD(content string) string {
+	content = trimTrailingNewlines(content)
 	longest := fence.LongestBacktickRun(content)
 	fenceLen := 3
 	if longest >= 3 {
 		fenceLen = longest + 2
 	}
-
 	f := strings.Repeat("`", fenceLen)
 	return fmt.Sprintf("%s\n%s\n%s\n", f, content, f)
 }
